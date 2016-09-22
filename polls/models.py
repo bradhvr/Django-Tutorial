bc4cb3c11726c22c9a2d1936df2101deffffebd8
Brad Hoover <bhoover@us.ibm.com>
2016-09-22 10:49:59 -0400

    Add tests

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.utils import timezone
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
@@ -12,8 +13,8 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 @python_2_unicode_compatible # only if you need to support Python 2
 class Choice(models.Model):
